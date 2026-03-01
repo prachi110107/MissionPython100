88556cbb103a190812c20db29d4263fff83dad42
Prachi Arora <prachiarora02899@gmail.com>
2026-03-01 17:49:28 +0530

    Day01

--- Day01.py
+++ Day01.py
@@ -1,1 +1,27 @@
-print("hello world")
+n = int(input("Enter number of rows: "))
+
+print("\n TRIANGLE :\n")
+for i in range(1, n + 1):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+
+print("\n PYRAMID :\n")
+for i in range(1, n + 1):
+    for j in range(n - i):
+        print(" ", end="")
+    for k in range(2 * i - 1):
+        print("*", end="")
+    
+    print()
+
+
+print("\n INVERTED PYRAMID :\n")
+for i in range(n, 0, -1):
+    for j in range(n - i):
+        print(" ", end="")
+    for k in range(2 * i - 1):
+        print("*", end="")
+    
+    print()
