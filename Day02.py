0611dc1391713a704feeb01c0c9f7e266708a2b0
Prachi Arora <prachiarora02899@gmail.com>
2026-03-02 21:37:20 +0530

    Day02.py

---
+++ Day02.py
@@ -0,0 +1,8 @@
+'''REVERSING A NUMBER'''
+n = int(input("Enter a number : "))
+reverse = 0
+while n > 0:
+    digit = n % 10     
+    reverse = reverse * 10 + digit
+    n = n // 10           
+print("Reversed Number is:", reverse)
