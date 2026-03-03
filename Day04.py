c179593519bb71acad45bc4241b93e726ac2869b
Prachi Arora <prachiarora02899@gmail.com>
2026-03-03 16:21:45 +0530

    Day04.py

--- Day04.py
+++ Day04.py
@@ -1,28 +1,12 @@
-value = input("Enter a number or string: ")
-if value.isdigit(): 
-    n = int(value)
-    original = n
-    reverse = 0
-    while n > 0:
-        digit = n % 10
-        reverse = reverse * 10 + digit
-        n = n // 10
-    if original == reverse:
-        print("It is a Palindrome Number")
-    else:
-        print("It is NOT a Palindrome Number")
-
+n = int(input("Enter a number : "))
+original = n
+digits = len(str(n))
+total = 0
+while n > 0:
+    digit = n % 10
+    total = total + digit ** digits
+    n = n // 10
+if total == original:
+    print("It is an Armstrong Number")
 else:
-    # For string
-    original = value
-    reverse = ""
-
-    # Reverse string using loop
-    for ch in original:
-        reverse = ch + reverse
-
-    # Check palindrome
-    if original == reverse:
-        print("It is a Palindrome String")
-    else:
-        print("It is NOT a Palindrome String")
+    print("It is NOT an Armstrong Number")
