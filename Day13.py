f7b0e1a300d360191be12baa8a8d16af363537a2
Prachi Arora <prachiarora02899@gmail.com>
2026-03-13 22:43:55 +0530

    Day13.py

---
+++ Day13.py
@@ -0,0 +1,15 @@
+'''Program to merge two lists'''
+n1 = int(input("Enter the number of elements of list1 : "))
+n2 = int(input("Enter the number of elements of list2 : "))
+
+list1 = []
+list2 = []
+for i in range(n1,n2):
+    num1 = int(input("Enter the elements : "))
+    num2 = int(input("Enter the elements : "))
+    list1.append(num1)
+    list2.append(num2)
+merged_list = list1+list2
+print("list1 : " , list1)
+print("list2 : " , list2)
+print("merged_list : " , merged_list)
