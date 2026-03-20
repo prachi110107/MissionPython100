a003a4d0af75cf1d6ed662ac9074342e91b26515
Prachi Arora <prachiarora02899@gmail.com>
2026-03-21 00:09:49 +0530

    Day20.py

--- Day19_LINEAR_SEARCH_USING_LIST.py
+++ Day19_LINEAR_SEARCH_USING_LIST.py
@@ -1,19 +1,38 @@
-'''Program to perform linear search'''
-n = int(input("Enter the number of elements : "))
-arr = []
-print("Enter elements : ")
-for i in range(n) :
-    num = int(input())
-    arr.append(num)
+'''
+Day19 :- Linear Search using list 
+Difficulty :- Easy
+Concepts :- Built-in functions
+Approach 
+Step 1 : Assume element not found (False)
+Step 2 : Traverse list
+Step 3 : If found → update to True
+Step 4 : Break loop
+Step 5 : After loop check flag
 
+'''
+
+# First input list
+lst = input("Enter elements separated by space: ").split()
+
+# Convert that input list into integers
+lst = [int(x) for x in lst]
+
+# Sort the list 
+lst.sort()
+
+# Enter element which you want to search 
 key = int(input("Enter the number which is to be found : "))
-found = False
 
-for i in range(n):
-    if arr[i] == key:
+# Use flag variable to check if element is found or not
+found = False   
+
+# Loop through list using index
+for i in range (len(lst)):                                   # Loop from index 0 to last index
+    if lst[i] == key:                                  # Check if current element matches the key
         print("Element found at position " , i + 1)
         found = True
         break
 
+# After loop, check if element was not found
 if not found:
     print("Element not found")   
