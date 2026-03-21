6379c82bf5b8d7646cfedf525156f59152748659
Prachi Arora <prachiarora02899@gmail.com>
2026-03-22 00:45:41 +0530

    Day08.py

--- Day08_TO_FIND_GCD_AND_LCM.py
+++ Day08_TO_FIND_GCD_AND_LCM.py
@@ -1,39 +1,35 @@
 '''
 Day08:- To find GCD and LCM 
 Difficulty:- Medium
-Concept:- Loops (while)
-• Conditional statements (if-else)
-• Euclidean Algorithm
-• Arithmetic operations
-• Integer division (//)
+Concept:- Loops , Conditional statements , Euclidean Algorithm , Arithmetic operations , Integer division (//)
 Approach:
-Step 1 :For GCD:
-Take two numbers
-Use repeated subtraction method
-Keep subtracting smaller from larger
-When both become equal → that is GCD
+For GCD:
+Step 1 : Take two numbers
+Step 2 : Use repeated subtraction method
+Step 3 : Keep subtracting smaller from larger
+Step 4 : When both become equal → that is GCD
 For LCM:
-
-Use formula:
-
-LCM = (n1 × n2) / GCD
+Use formula : LCM = (n1 * n2) // GCD
 
  '''
-n1 = int(input("Enter first number : "))
-n2 = int(input("Enter second number : "))
+n1 = int(input("Enter first number : "))          # Ask user to input first number 
+n2 = int(input("Enter second number : "))         # Ask user to input second number
 
+# Store original values
 a = n1
 b = n2
 
-while a != b:
-    if a > b:
+while a != b:                                      # Loop runs until both a and b becomes equal
+    if a > b:                                      # If a is greater
         a = a - b
-    else:
+    else:                                          # If b is greater
         b = b - a
 
-gcd = a  
+gcd = a                                            # when loop ends both a and b are  equal -> GCD
 
-lcm = (n1 * n2) // gcd
+lcm = (n1 * n2) // gcd                             # To Calculate LCM 
 
+
+# Print results
 print(f"GCD of {n1} and {n2} is: {gcd}")
 print(f"LCM of {n1} and {n2} is: {lcm}")
