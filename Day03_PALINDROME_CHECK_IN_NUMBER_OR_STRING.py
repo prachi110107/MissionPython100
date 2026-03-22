e959ab433f807793ebd1e7418f0e7cf2ba5fbcca
Prachi Arora <prachiarora02899@gmail.com>
2026-03-22 23:13:05 +0530

    Day03.py

--- Day03_PALINDROME_CHECK_IN_NUMBER_OR_STRING.py
+++ Day03_PALINDROME_CHECK_IN_NUMBER_OR_STRING.py
@@ -14,29 +14,34 @@ Compare with original
 
  '''
 
+# Take input from user it can be a number or string
 Value = input("Enter a number or string : ")
+
+# Check if it is a number 
 if Value.isdigit():
 
-    n = int(Value)
+    n = int(Value)                  # If yes , then convert it into an integer
     original = n
     reverse = 0
 
-    while n > 0:
+    while n > 0:                    # Reverse number concept
         digit = n % 10     
         reverse = reverse * 10 + digit
         n = n // 10   
 
-    if original == reverse :
+    if original == reverse :        # Compare original and reverse
         print("It is a palindrome number")
     else:
         print("It is NOT a palindrome number")
 
-else:
+else:                               # If input is a string
     original = Value
     reverse = "" 
-    for ch in original:
-        reverse = ch + reverse
+    for ch in original:              # Reverse string using loop
+        reverse = ch + reverse       # add character in front
         
+
+# Compare reverse and original       
     if original == reverse:
         print("It is a Palindrome String")
     else:
