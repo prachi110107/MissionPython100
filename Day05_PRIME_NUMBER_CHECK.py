c65aa18a52e76a794c9f3b3e6116cc5424c81a7a
Prachi Arora <prachiarora02899@gmail.com>
2026-03-22 22:40:50 +0530

    Day05.py

--- Day05_PRIME_NUMBER_CHECK.py
+++ Day05_PRIME_NUMBER_CHECK.py
@@ -1,12 +1,22 @@
-'''Day14:- Matrix addition
-Difficulty:- Easy 
-Concept:- 2D Lists , Nested loops
+'''
+Day05:- Prime number check
+Difficulty:- Medium
+Concept:- A Prime Number is a number that is divisible only by: 1 and itself
 Approach:
-Step 1 : '''
+Step 1 : Take number from user
+Step 2 : Loop from 2 to n-1
+Step 3 : Check if number is divisible
+         If divisible → not prime
+         If no divisor found → prime
 
+'''
+
+# Ask user to enter a number
 n=int(input("Enter number of your choice : "))
-for i in range(2,n):
-    if n % i == 0:
-        print("It's not a prime number")
-else:
-    print("It's a prime number")
+
+for i in range(2,n):                                # Loop runs from 2 to n-1
+    if n % i == 0:                                  # Check if n divisble by i or not 
+        print("It's not a prime number")            # If divisible , then not a prime number
+
+else:                                               # If not divisible , then is a prime number
+    print("It's a prime number")              
--- Day06_TO_FIND_FACTORIAL.py
+++ Day06_TO_FIND_FACTORIAL.py
@@ -1,20 +1,32 @@
-'''Day14:- Matrix addition
+'''
+Day06:- To find factorial of a given number
 Difficulty:- Easy 
-Concept:- 2D Lists , Nested loops
+Concept:- Factorial of a number means:
+          n! = n × (n-1) × (n-2) × ... × 1
 Approach:
-Step 1 : '''
+Step 1 : Take input number from user
+Step 2 : Check special cases:
+         If number < 0 → factorial does not exist
+         If number = 0 → factorial is 1
+Step 3 : Otherwise:
+Initialize factorial = 1
+Use loop from 1 to n
+Multiply each number
+Step 4 : Print final result
 
-n=int(input("Enter a number : "))
+'''
 
-if n < 0 :
+n=int(input("Enter a number : "))                   # Ask user to enter a number
+
+if n < 0 :                                          # If entered number less then 0 then factorial does not exists
     print("Factorial does not exist")
 
-elif n == 0 :
+elif n == 0 :                                       # If entered number is zero than factorial is one
     print("Factorial of zero is 1")
 
-else:
+else:                                              # If entered number is greater than 1
     factorial = 1
     for i in range (1,n +1):
         factorial = factorial * i
         
-    print("Factorial of" , n , "is" , factorial)
+    print("Factorial of" , n , "is" , factorial)   # print factorial
