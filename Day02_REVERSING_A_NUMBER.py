db11e085b652463349cab8c7bb456322a4101748
Prachi Arora <prachiarora02899@gmail.com>
2026-03-22 23:07:43 +0530

    Day02.py

--- Day02_REVERSING_A_NUMBER.py
+++ Day02_REVERSING_A_NUMBER.py
@@ -12,12 +12,15 @@ Step 4 : Print reversed number
 
 '''
 
-
+# Ask user to enter a number   
 n = int(input("Enter a number : "))
 
-reverse = 0
-while n > 0:
-    digit = n % 10     
-    reverse = reverse * 10 + digit
-    n = n // 10           
+reverse = 0                                     # Initialize reverse
+
+while n > 0:                                    # Reverse logic using while loop
+    digit = n % 10                              # Get last digit
+    reverse = reverse * 10 + digit              # Build reverse
+    n = n // 10                                 # Remove last digit
+
+# Print reverse number
 print("Reversed Number is:", reverse)
