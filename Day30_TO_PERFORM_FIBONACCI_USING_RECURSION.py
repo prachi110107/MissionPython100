ad825f8956182224374e7065f98b5f8121e7b5e6
Prachi Arora <prachiarora02899@gmail.com>
2026-03-30 16:41:35 +0530

    Day30.py

--- Day30_TO_PERFORM_FIBONACCI_USING_RECURSION.py
+++ Day30_TO_PERFORM_FIBONACCI_USING_RECURSION.py
@@ -5,50 +5,50 @@ Concepts :-
 A Palindrome is a string that reads same forward and backward
 Using recursion: Compare first and last character , Then check remaining string
 Approach 
-Step 1 : Take number or string input
+Step 1 : Take number of terms from user
 Step 2 : Define recursive function
-Step 3 : Base case: If string length ≤ 1 → palindrome
-Step 4 : Check: First char == last char
-Step 5 : Recursive call on remaining string
-Step 6 : Print result
-
+Step 3 : Base case:
+         if n == 0 → return 0
+         if n == 1 → return 1
+Step 4 : Recursive case:
+         return fib(n-1) + fib(n-2)
+Step 5 : Use loop to print series
 
 '''
 
-def palindrome(n) :                                 # Defining recursive function
+def fibonacci(n) :                                 # Defining recursive function
 
-    if len(n) <= 1:                                 # Base case , palindrome
-        return True
+    if n == 0  :                                   # Base case 
+        return 0
     
-    if n[0] != n[-1]:                               # Check first and last character , not palindrome
-        return False                 
+    if n == 1  :                                   
+        return 1                
      
-    return palindrome(n[1:-1])                      # Recursive call 
+    return fibonacci(n-1) + fibonacci(n-2)         # Recursive call 
 
 
-# Take number of inputs from user
-n=int(input("Enter number of terms : "))
 
-# Initialize first two Fibonacci numbers
-a=0          # first term
-b=1          # second term
+n=int(input("Enter number of terms : "))           # Take number of inputs from user
 
-# Check for invalid input
-if n <= 0 :
+                                                   # Initialize first two Fibonacci numbers
+a=0                                                # first term
+b=1                                                # second term
+
+
+if n <= 0 :                                        # If entered number is negative
     print("Enter a positive number")
 
-# If only one term is required
-elif n == 1:
-    print("Fibonacci series")
-    print(a)  # Print only the first term
 
-# If more than one term
-else:
+elif n == 1:                                       # If only one term is required
+    print("Fibonacci series")
+    print(a)                                       # Print only the first term
+
+else:                                              # If more than one term
     print("fibonacci series")
-    for i in range (n):             # Loop runs n times 
-        print(a)                    # Print current term
-        c=a+b                       # Print next term
+    for i in range (n):                            # Loop runs n times 
+        print(a)                                   # Print current term
+        c=a+b                                      # Print next term
        
-       # Update values
-        a=b                         # Shift b to a
-        b=c                         # Shift c to b
+                                                   # Update values
+        a=b                                        # Shift b to a
+        b=c                                        # Shift c to b
