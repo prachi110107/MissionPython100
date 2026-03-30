81bd64ee97e6fb8d06d861fbdcf6c0630c29536f
Prachi Arora <prachiarora02899@gmail.com>
2026-03-30 16:50:47 +0530

    Day30.py

--- Day28_REVERSING_A_STRING_USING_RECURSION.py
+++ Day28_REVERSING_A_STRING_USING_RECURSION.py
@@ -14,7 +14,6 @@ Step 5 : Print result
 
 '''
 
-
 string = input("Enter a string : ")              # Take input from user
 
 def reverse_string(str):                         # Define recursive function
