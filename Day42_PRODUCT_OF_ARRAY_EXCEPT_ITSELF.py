249f1be7ee3c9157c576a4af9145e92fe61c4f50
Prachi Arora <prachiarora02899@gmail.com>
2026-05-13 15:07:33 +0530

    Day42.py

--- Day42_PRODUCT_OF_ARRAY_EXCEPT_ITSELF.py
+++ Day42_PRODUCT_OF_ARRAY_EXCEPT_ITSELF.py
@@ -2,21 +2,34 @@
 Day42:- Product Of Array Except Itself
 Difficulty:- Hard
 Concept:- Arrays / Lists , Nested Loop , Multiplication Logic
-✅ Approach
-
-1️⃣ Take array elements as input from the user.
-
-2️⃣ Traverse each index of the array.
-
-3️⃣ For every element, multiply all other elements except itself.
-
-4️⃣ Store the product in a new list.
-
-5️⃣ Print the final result list.
-Step 1 : Use two pointers (left and right) to create a sliding window.
-Step 2 :  Store frequency of characters in a dictionary.
-Step 3 :  Find : window size - maximum frequency character
-Step 4 :  If replacements needed become greater than k, shrink the window from the left side.
-Step 5 :  Continuously update the maximum valid window length.
+Approach:
+Step 1 : Take array elements as input from the user.
+Step 2 : Traverse each index of the array.
+Step 3 : For every element, multiply all other elements except itself.
+Step 4 : Store the product in a new list.
+Step 5 : Print the final result list.
 
 '''
+
+# Product of Array Except Itself
+
+numbers = input("Enter numbers separated by space: ")
+
+# convert into integer list
+numbers = [int(i) for i in numbers.split()]
+
+result = []
+
+for i in range(len(numbers)):
+
+    product = 1
+
+    for j in range(len(numbers)):
+
+        if i != j:
+            product = product * numbers[j]
+
+    result.append(product)
+
+print("Product array except itself is:")
+print(result)
