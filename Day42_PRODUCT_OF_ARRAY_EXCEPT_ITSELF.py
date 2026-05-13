588e1f009a7801c77b9f8418ee8cb85d5df1df0f
Prachi Arora <prachiarora02899@gmail.com>
2026-05-13 15:03:57 +0530

    Day40.py

--- Day42_PRODUCT_OF_ARRAY_EXCEPT_ITSELF.py
+++ Day42_PRODUCT_OF_ARRAY_EXCEPT_ITSELF.py
@@ -1,7 +1,18 @@
 '''
 Day42:- Product Of Array Except Itself
 Difficulty:- Hard
-Concept:- Sliding Window , Hash Map , Dictionary , Frequency Counting
+Concept:- Arrays / Lists , Nested Loop , Multiplication Logic
+✅ Approach
+
+1️⃣ Take array elements as input from the user.
+
+2️⃣ Traverse each index of the array.
+
+3️⃣ For every element, multiply all other elements except itself.
+
+4️⃣ Store the product in a new list.
+
+5️⃣ Print the final result list.
 Step 1 : Use two pointers (left and right) to create a sliding window.
 Step 2 :  Store frequency of characters in a dictionary.
 Step 3 :  Find : window size - maximum frequency character
