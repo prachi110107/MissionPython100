60144e47bcbed2853c4ae1466bbf7368a41aca05
Prachi Arora <prachiarora02899@gmail.com>
2026-05-14 15:29:09 +0530

    Day49.py

--- Day49_CHECK_BALANCED_BINARY_TREE.py
+++ Day49_CHECK_BALANCED_BINARY_TREE.py
@@ -1,12 +1,64 @@
 '''
 Day49:- Check balanced binary tree
 Difficulty:- Hard
-Concept:- Arrays / Lists , Nested Loop , Multiplication Logic
+Concept:- Binary Trees , Recursion , Height Calculation
 Approach:
-Step 1 : Take array elements as input from the user.
-Step 2 : Traverse each index of the array.
-Step 3 : For every element, multiply all other elements except itself.
-Step 4 : Store the product in a new list.
-Step 5 : Print the final result list.
+Step 1 : Find height of left subtree.
+Step 2 : Find height of right subtree.
+Step 3 : Calculate difference : abs(left_height - right_height)
+Step 4 : If difference is greater than 1 : Tree is not balanced
+Step 5 : Recursively check left and right subtrees.
 
 '''
+
+# Check Balanced Binary Tree
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+# Function to find height
+def height(root):
+
+    if root is None:
+        return 0
+
+    left = height(root.left)
+    right = height(root.right)
+
+    return max(left, right) + 1
+
+
+# Function to check balance
+def isBalanced(root):
+
+    if root is None:
+        return True
+
+    left_height = height(root.left)
+    right_height = height(root.right)
+
+    if abs(left_height - right_height) > 1:
+        return False
+
+    return isBalanced(root.left) and isBalanced(root.right)
+
+
+# Creating Tree
+root = Node(1)
+
+root.left = Node(2)
+root.right = Node(3)
+
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+# Check balance
+if isBalanced(root):
+    print("Balanced Binary Tree")
+else:
+    print("Not Balanced")
