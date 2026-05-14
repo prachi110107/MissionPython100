d09c6190c2bf4583b35c5e7819dd1a04d87bd990
Prachi Arora <prachiarora02899@gmail.com>
2026-05-14 15:33:34 +0530

    Day50.py

--- Day50_LOWEST_COMMON_ANCESTO_FOR_A_BINARY_TREE.py
+++ Day50_LOWEST_COMMON_ANCESTO_FOR_A_BINARY_TREE.py
@@ -1,12 +1,62 @@
 '''
 Day50:- Lowest common ancesto for a binary tree
 Difficulty:- Hard
-Concept:- Arrays / Lists , Nested Loop , Multiplication Logic
+Concept:- Binary tree , recursion , tree traversal
 Approach:
-Step 1 : Take array elements as input from the user.
-Step 2 : Traverse each index of the array.
-Step 3 : For every element, multiply all other elements except itself.
-Step 4 : Store the product in a new list.
-Step 5 : Print the final result list.
+Step 1 : Start traversal from root node.
+Step 2 : If root becomes : node1 , node2 , then return root.
+Step 3 : Recursively search in : left subtree , right subtree
+Step 4 : If both sides return values : current root is LCA
+Step 5 : Otherwise return non-null subtree result.
 
 '''
+
+# Lowest Common Ancestor in Binary Tree
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+# LCA Function
+def lca(root, n1, n2):
+
+    if root is None:
+        return None
+
+    if root.data == n1 or root.data == n2:
+        return root
+
+    left = lca(root.left, n1, n2)
+    right = lca(root.right, n1, n2)
+
+    # if both found
+    if left and right:
+        return root
+
+    # otherwise return non-null value
+    if left:
+        return left
+    else:
+        return right
+
+
+# Creating Tree
+root = Node(1)
+
+root.left = Node(2)
+root.right = Node(3)
+
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+root.right.left = Node(6)
+root.right.right = Node(7)
+
+
+result = lca(root, 4, 5)
+
+print("Lowest Common Ancestor is:", result.data)
