729c5bacde1b84d30fdc865c84d2c9f28d757b46
Prachi Arora <prachiarora02899@gmail.com>
2026-05-14 16:12:28 +0530

    Day54.py

--- Day54_CHECK_IF_TWO_TREES_ARE_IDENTICAL_USING_RECURSION.py
+++ Day54_CHECK_IF_TWO_TREES_ARE_IDENTICAL_USING_RECURSION.py
@@ -8,3 +8,32 @@ Step 2 : Recursively compare : left subtree , right subtree
 Step 3 : Return True if all nodes match
 
 '''
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def identical(root1, root2):
+
+    if root1 is None and root2 is None:
+        return True
+
+    if root1 is None or root2 is None:
+        return False
+
+    return (root1.data == root2.data and
+            identical(root1.left, root2.left) and
+            identical(root1.right, root2.right))
+
+
+root1 = Node(1)
+root1.left = Node(2)
+
+root2 = Node(1)
+root2.left = Node(2)
+
+print(identical(root1, root2))
