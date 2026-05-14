a4835a415a602b3547a60d36dd9e07f8ad20daf8
Prachi Arora <prachiarora02899@gmail.com>
2026-05-14 18:09:35 +0530

    Day68.py

--- Day68_TOPOLOGICAL_SORTING.py
+++ Day68_TOPOLOGICAL_SORTING.py
@@ -1,8 +1,43 @@
 '''
 Day68:- Topological sorting
 Difficulty:- Hard
-Concept:- BFS , HashMap / Dictionary , Horizontal Distance
+Concept:- DFS , Stack , DAG
 Approach:
-Step 1 : 
+Step 1 : Perform DFS
+Step 2 : Push nodes into stack after recursion
+Step 3 : Reverse stack gives topological order
 
 '''
+
+graph = {
+    5:[2,0],
+    4:[0,1],
+    2:[3],
+    3:[1],
+    1:[],
+    0:[]
+}
+
+visited = set()
+
+stack = []
+
+def dfs(node):
+
+    visited.add(node)
+
+    for neighbor in graph[node]:
+
+        if neighbor not in visited:
+            dfs(neighbor)
+
+    stack.append(node)
+
+for node in graph:
+
+    if node not in visited:
+        dfs(node)
+
+stack.reverse()
+
+print(stack)
--- Day69_TOPOLOGICAL_SORT_USING_BFS_(KHAN'S ALGORITHM).py
+++ Day69_TOPOLOGICAL_SORT_USING_BFS_(KHAN'S ALGORITHM).py
@@ -1,7 +1,10 @@
 '''
 Day69:- Topological sort using BFS (khan's algorithm)
 Difficulty:- Hard
-Concept:- BFS , HashMap / Dictionary , Horizontal Distance
+Concept:- BFS , Queue , Indegree
 Approach:
-Step 1 : 
+Step 1 : Calculate indegree of every node
+Step 1 : Insert nodes with indegree 0 into queue
+Step 1 : Remove node and reduce indegree of neighbors
+
 '''
