2896a6ac21c24b5c0beddf80f72138301b35afd5
Prachi Arora <prachiarora02899@gmail.com>
2026-05-14 18:04:47 +0530

    Day67.py

--- Day67_DETECTING_CYCLES_IN_A_DIRECTED_GRAPG_USING_BACKTRACKING_AND_DF_CONCEPT.py
+++ Day67_DETECTING_CYCLES_IN_A_DIRECTED_GRAPG_USING_BACKTRACKING_AND_DF_CONCEPT.py
@@ -8,3 +8,34 @@ Step 2 : Use recursion stack
 Step 3 : If node already in recursion stack : cycle exists
 
 '''
+graph = {
+    0:[1],
+    1:[2],
+    2:[0]
+}
+
+visited = set()
+
+path = set()
+
+def dfs(node):
+
+    visited.add(node)
+
+    path.add(node)
+
+    for neighbor in graph[node]:
+
+        if neighbor not in visited:
+
+            if dfs(neighbor):
+                return True
+
+        elif neighbor in path:
+            return True
+
+    path.remove(node)
+
+    return False
+
+print(dfs(0))
