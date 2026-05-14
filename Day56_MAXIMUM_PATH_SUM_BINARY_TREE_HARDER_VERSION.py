8d486ec8c31268f29fe4032c9c856c3ca29c4b86
Prachi Arora <prachiarora02899@gmail.com>
2026-05-14 16:16:08 +0530

    Day56.py

--- Day56_MAXIMUM_PATH_SUM_BINARY_TREE_HARDER_VERSION.py
+++ Day56_MAXIMUM_PATH_SUM_BINARY_TREE_HARDER_VERSION.py
@@ -1,8 +1,51 @@
 '''
 Day56:- Maximum path sum binary tree harder version
 Difficulty:- Hard
-Concept:- Binary tree , recursion , tree traversal
+Concept:- Recursion , DFS , Dynamic Path Calculation
 Approach:
-Step 1 : 
+Step 1 : Find maximum left path sum
+Step 2 : Find maximum right path sum
+Step 3 : Ignore negative sums
+Step 4 : Update global maximum path sum
 
 '''
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+maximum = float("-inf")
+
+
+def pathSum(root):
+
+    global maximum
+
+    if root is None:
+        return 0
+
+    left = max(pathSum(root.left), 0)
+    right = max(pathSum(root.right), 0)
+
+    current = root.data + left + right
+
+    maximum = max(maximum, current)
+
+    return root.data + max(left, right)
+
+
+root = Node(10)
+
+root.left = Node(2)
+root.right = Node(10)
+
+root.left.left = Node(20)
+root.left.right = Node(1)
+
+pathSum(root)
+
+print("Maximum Path Sum:", maximum)
