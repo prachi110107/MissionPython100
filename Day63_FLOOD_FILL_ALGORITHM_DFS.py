451cee1e283b6a6cd22ad09854b0ffa124d6dac7
Prachi Arora <prachiarora02899@gmail.com>
2026-05-14 16:57:42 +0530

    Day63.py

--- Day63_FLOOD_FILL_ALGORITHM_DFS.py
+++ Day63_FLOOD_FILL_ALGORITHM_DFS.py
@@ -8,3 +8,37 @@ Step 2 : Change current color
 Step 3 : Recursively fill neighboring cells
 
 '''
+
+image = [
+    [1,1,1],
+    [1,1,0],
+    [1,0,1]
+]
+
+sr = 1
+sc = 1
+newColor = 2
+
+old = image[sr][sc]
+
+rows = len(image)
+cols = len(image[0])
+
+def dfs(r, c):
+
+    if r < 0 or c < 0 or r >= rows or c >= cols:
+        return
+
+    if image[r][c] != old:
+        return
+
+    image[r][c] = newColor
+
+    dfs(r+1, c)
+    dfs(r-1, c)
+    dfs(r, c+1)
+    dfs(r, c-1)
+
+dfs(sr, sc)
+
+print(image)
