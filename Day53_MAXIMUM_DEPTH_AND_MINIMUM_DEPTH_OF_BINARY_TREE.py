750de69efd77f810d925a2ca6aab63b9a5ec3aa0
Prachi Arora <prachiarora02899@gmail.com>
2026-05-14 16:11:46 +0530

    Day53.py

--- Day53_MAXIMUM_DEPTH_AND_MINIMUM_DEPTH_OF_BINARY_TREE.py
+++ Day53_MAXIMUM_DEPTH_AND_MINIMUM_DEPTH_OF_BINARY_TREE.py
@@ -8,3 +8,37 @@ Step 2 : Maximum depth : max(left, right) + 1
 Step 3 : Minimum depth : min(left, right) + 1
 
 '''
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def maxDepth(root):
+
+    if root is None:
+        return 0
+
+    return max(maxDepth(root.left),
+               maxDepth(root.right)) + 1
+
+
+def minDepth(root):
+
+    if root is None:
+        return 0
+
+    return min(minDepth(root.left),
+               minDepth(root.right)) + 1
+
+
+root = Node(1)
+
+root.left = Node(2)
+root.right = Node(3)
+
+print("Maximum Depth:", maxDepth(root))
+print("Minimum Depth:", minDepth(root))
