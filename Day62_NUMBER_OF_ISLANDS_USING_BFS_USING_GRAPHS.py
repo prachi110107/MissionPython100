c4164394c301d5ca99517fb0bcfba6b25be5a73f
Prachi Arora <prachiarora02899@gmail.com>
2026-05-14 16:56:46 +0530

    Day62.py

--- Day62_NUMBER_OF_ISLANDS_USING_BFS_USING_GRAPHS.py
+++ Day62_NUMBER_OF_ISLANDS_USING_BFS_USING_GRAPHS.py
@@ -8,3 +8,52 @@ Step 2 : If land found : increment count , perform BFS using queue
 Step 3 : Mark visited cells
 
 '''
+
+from collections import deque
+
+grid = [
+    ["1","1","0"],
+    ["1","0","0"],
+    ["0","1","1"]
+]
+
+rows = len(grid)
+cols = len(grid[0])
+
+count = 0
+
+def bfs(r, c):
+
+    queue = deque()
+
+    queue.append((r, c))
+
+    grid[r][c] = "0"
+
+    while queue:
+
+        row, col = queue.popleft()
+
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
+
+        for dr, dc in directions:
+
+            nr = row + dr
+            nc = col + dc
+
+            if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == "1":
+
+                queue.append((nr, nc))
+                grid[nr][nc] = "0"
+
+
+for i in range(rows):
+
+    for j in range(cols):
+
+        if grid[i][j] == "1":
+
+            count += 1
+            bfs(i, j)
+
+print("Number of islands:", count)
