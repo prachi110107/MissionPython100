a99260f99e88a16086db1a2a4f4b3991fbf1e20d
Prachi Arora <prachiarora02899@gmail.com>
2026-05-14 16:10:53 +0530

    Day52.py

--- Day52_PATH_SUM_ROOT_TO_LEAF.py
+++ Day52_PATH_SUM_ROOT_TO_LEAF.py
@@ -10,3 +10,33 @@ Step 4 : Return True or False
 
 
 '''
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def pathSum(root, target):
+
+    if root is None:
+        return False
+
+    if root.left is None and root.right is None:
+        return target == root.data
+
+    target = target - root.data
+
+    return pathSum(root.left, target) or pathSum(root.right, target)
+
+
+root = Node(5)
+
+root.left = Node(4)
+root.right = Node(8)
+
+root.left.left = Node(11)
+
+print(pathSum(root, 20))
